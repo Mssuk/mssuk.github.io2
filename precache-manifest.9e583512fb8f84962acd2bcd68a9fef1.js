31870ff741631c1b978d84e5bc897ce868f1f54f
mssuk <djqhr490@gmail.com>
2019-03-13 17:33:55 +0900

    jebal

---
+++ precache-manifest.9e583512fb8f84962acd2bcd68a9fef1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "88f0ca9b60eec35875dd5e115885038a",
+    "url": "/static/media/obs.88f0ca9b.png"
+  },
+  {
+    "revision": "31c375ddbb9592cb96d6e7e401ba81e1",
+    "url": "/static/media/biman.31c375dd.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "644cae6237278dcd2548",
+    "url": "/static/js/main.74f19fb7.chunk.js"
+  },
+  {
+    "revision": "ffb55593b75ee1e25137",
+    "url": "/static/js/2.1c473e9d.chunk.js"
+  },
+  {
+    "revision": "644cae6237278dcd2548",
+    "url": "/static/css/main.6420b494.chunk.css"
+  },
+  {
+    "revision": "ffb55593b75ee1e25137",
+    "url": "/static/css/2.4c3bdd24.chunk.css"
+  },
+  {
+    "revision": "fb06a18c55a1a7ffbe0099df9f284f28",
+    "url": "/index.html"
+  }
+];
